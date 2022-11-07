955d61cfff88bf6bb7a0f79a81f12abaa84aff88
Panisara Sonkom <[psonkom342@gmail.com]>
2022-11-08 05:02:22 +1100

    create user table

--- app.py
+++ app.py
@@ -7,6 +7,23 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://scuba_dev:spam123
 # create new instance; connects everything together and return a db object
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(70))
+    contact_number = db.Column(db.Integer)
+    dive_level = db.Column(db.String)
+    DoB = db.Column(db.Date)
+
+#create table in psql database
+#Define a custom CLI (terminal) command
+@app.cli.command('create')
+def create_db():
+    db.create_all()
+    print("Tables created")
+
+
+
 @app.route('/')
 def index():
     return "Hello, world!"
