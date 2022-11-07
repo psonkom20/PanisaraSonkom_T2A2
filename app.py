9b0b29f1680d0e6015db53727eb1f0ebecb0dc22
Panisara Sonkom <[psonkom342@gmail.com]>
2022-11-08 03:57:17 +1100

    Connect to database

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app= Flask(__name__)
+# set string to connect to database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://scuba_dev:spam123@127.0.0.1:5432/scuba_dive_shop'
+# create new instance; connects everything together and return a db object
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
